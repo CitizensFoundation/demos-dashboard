d1feaf3f36fd4e29b80bd0b41dd3397dedd5248d
Robert Bjarnason <robert.bjarnason@gmail.com>
2022-03-06 18:16:58 +0000

    Work

--- web-app/dist/sw.js
+++ web-app/dist/sw.js
@@ -27,7 +27,7 @@ self.addEventListener('message', (event) => {
 self.__precacheManifest = [
   {
     "url": "index.html",
-    "revision": "af298b91202a3a908d224b4359418897"
+    "revision": "b74a0c7a9958775d2379ab8dba1be570"
   },
   {
     "url": "polyfills/custom-elements-es5-adapter.84b300ee818dce8b351c7cc7c100bcf7.js",
@@ -42,8 +42,8 @@ self.__precacheManifest = [
     "revision": "939ef49522c9d6762c23f0a3fb10e87b"
   },
   {
-    "url": "your-grievances-app-6f86566f.js",
-    "revision": "cb5512c62f0821108f21b365d3938bd4"
+    "url": "your-grievances-app-c80424e2.js",
+    "revision": "a3eb4057491e10201f4a34023c9bb58d"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
